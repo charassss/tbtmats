0932fcb1eb195814f32b3e16262f036f63d1af5a
charassss <chara_ss@outlook.com>
2022-05-30 14:18:35 +0800

    version1.0.1 2022/5/30 added compressed_workbench to my mod

--- src/main/java/me/summerunni/tbtmats/init/ModRecipes.java
+++ src/main/java/me/summerunni/tbtmats/init/ModRecipes.java
@@ -6,8 +6,11 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 public class ModRecipes {
 
     public static void init() {
+        //register recipes
         GameRegistry.addSmelting(ModBlocks.AGC_ORE, new ItemStack(ModItems.AGC_INGOT), 1.0f);
         GameRegistry.addSmelting(ModBlocks.BX_ORE, new ItemStack(ModItems.BX_INGOT), 1.0f);
+        //delete recipes
+
     }
 
 }
